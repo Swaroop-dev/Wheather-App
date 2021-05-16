52ec37949621ad87d51279e0aa197589ad9733a2
Swaroop-dev <65998110+Swaroop-dev@users.noreply.github.com>
2021-05-16 23:38:15 +0530

    no  async needed

--- src/Components/apiCalls.js
+++ src/Components/apiCalls.js
@@ -2,13 +2,13 @@
 const API = "https://www.metaweather.com/api/location/search/?query=";
 const API1 = "https://www.metaweather.com/api/location/";
 
-export const search = async (word) => {
+export const search =  (word) => {
   let changeUrl = API + word;
 
   return fetch(changeUrl).then((res) => res.json());
 };
 
-export const searchCity = async (woeid) => {
+export const searchCity = (woeid) => {
   console.log(woeid);
   let url = API1 +woeid; 
   return fetch(url).then((res) => res.json());
